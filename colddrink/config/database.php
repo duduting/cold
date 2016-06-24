c9a49b51e18c89df92414abf4785313c05fe5f0b
duduting <937627140@qq.com>
2016-06-24 18:51:20 +0800

    1111

--- colddrink/config/database.php
+++ colddrink/config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => 'localhost',
             'database'  => 'weixin',
             'username'  => 'root',
-            'password'  => '',
+            'password'  => 'root',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
